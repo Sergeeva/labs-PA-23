538b068da2f12c22e84c5df261803f7bb5734e66
lena <srgv.lena@gmail.com>
2023-09-06 12:05:41 +0300

    added examples with process and thread creation to dir task00

---
+++ task00/thread/hello_thread.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <thread>
+
+
+void my_thread_func(){
+  std::cout<<"Hello from my thread! "<< a << std::endl;
+}
+
+int main(){
+
+  std::thread my_t(my_thread_func);//запуск потока
+
+  std::cout<<"Hello from main! "<<std::endl;
+  
+  my_t.join();
+  
+  return 0;
+}
