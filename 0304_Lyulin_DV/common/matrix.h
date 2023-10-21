40e12c0cdd26134bf254a3f6642e649bbb83517f
Astana-Mirza <lyulin.2003@mail.ru>
2023-10-21 13:43:50 +0300

    added lab4

--- 0304_Lyulin_DV/common/matrix.h
+++ 0304_Lyulin_DV/common/matrix.h
@@ -3,41 +3,52 @@
 
 #include <vector>
 #include <ostream>
+#include <thread>
 
 class Matrix
 {
 public:
-	static constexpr int output_precision = 3;
+     static constexpr int output_precision = 3;
+     static constexpr float compare_precision = 0.001;
+     friend bool operator==( const Matrix&, const Matrix& );
 
-	Matrix( size_t rows = 0, size_t cols = 0 );
-	Matrix( const Matrix& other ) = default;
-	Matrix( Matrix&& other ) = default;
-	Matrix& operator=( const Matrix& rhs ) = default;
-	Matrix& operator=( Matrix&& rhs ) = default;
+     Matrix( size_t rows = 0, size_t cols = 0 );
+     Matrix( const Matrix& other ) = default;
+     Matrix( Matrix&& other ) = default;
+     Matrix& operator=( const Matrix& rhs ) = default;
+     Matrix& operator=( Matrix&& rhs ) = default;
 
-	float get( size_t i, size_t j ) const;
-	float& get( size_t i, size_t j );
+     float get( size_t i, size_t j ) const;
+     float& get( size_t i, size_t j );
 
-	size_t get_cols() const;
-	size_t get_rows() const;
+     size_t get_cols() const;
+     size_t get_rows() const;
 
-	const float *data() const;
-	float *data();
+     void insert_part( const Matrix& matr, size_t row, size_t col );
+     const float *data() const;
+     float *data();
 
-	static Matrix generate( size_t i, size_t j );
+     static Matrix generate( size_t i, size_t j );
 
 private:
-	static constexpr float rand_min = -2.0f;
-	static constexpr float rand_max = 2.0f;
+     static constexpr float rand_min = -2.0f;
+     static constexpr float rand_max = 2.0f;
 
-	size_t rows_;
-	size_t cols_;
-	std::vector< float > data_;
+     size_t rows_;
+     size_t cols_;
+     std::vector< float > data_;
 };
 
 
 Matrix operator*( const Matrix& lhs, const Matrix& rhs );
 std::ostream& operator<<( std::ostream& lhs, const Matrix& rhs );
+bool operator==( const Matrix& lhs, const Matrix& rhs );
+
+Matrix multiply_matrices_strassen( const Matrix& lhs, const Matrix& rhs );
+Matrix multiply_matrices_parallel_v1( const Matrix& lhs, const Matrix& rhs,
+     size_t thread_count = static_cast< size_t >( std::thread::hardware_concurrency() ) );
+Matrix multiply_matrices_parallel_v2( const Matrix& lhs, const Matrix& rhs,
+     size_t thread_count = static_cast< size_t >( std::thread::hardware_concurrency() ) );
 
 #endif // MATRIX_H
 
