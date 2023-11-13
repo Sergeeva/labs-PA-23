163e9694422b9d477c97a3d9fc6c7278c2cbbdf5
Ekaterina0303 <71796000+Ekaterina0303@users.noreply.github.com>
2023-11-14 01:16:05 +0300

    lab3

---
+++ 0303_Kurochkina_EA/lb3/src/matrix.h
@@ -0,0 +1,33 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+#include<iostream>
+#include<vector>
+#include<thread>
+#include<fstream>
+#define MULT_THREAD_COUNT 4
+
+using namespace std;
+
+int sum(const vector<vector<int>>& left, const vector<vector<int>>& right, int i, int j);
+
+void parallelMult(const vector<vector<int>>& left,  const vector<vector<int>>& right, vector<vector<int>>& res, const int thread_count, const int num);
+
+class Matrix {
+public:
+    vector<vector<int>> matrix;
+    int rows, cols;
+    Matrix(int r, int c);
+    Matrix() = default;
+    Matrix operator* (const Matrix& other);
+    Matrix(const Matrix& other);
+    Matrix(const Matrix&& other);
+
+    void operator= (const Matrix&& other);
+
+    void printMatrix();
+
+    friend std::ostream& operator<< (std::ostream& stream, const Matrix& m);
+
+};
+
+#endif // MATRIX_H
