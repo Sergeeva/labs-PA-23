83c2d77584ff065c18c5aa4ae3740922fdf64bed
Kostebelova-Elizaveta <Lizbet227@gmail.com>
2023-12-12 15:06:19 +0200

    Change some files

--- 0303_Kostebelova_EK/lab5/PA_lab5/main.cpp
+++ 0303_Kostebelova_EK/lab5/PA_lab5/main.cpp
@@ -1,70 +1,73 @@
-﻿#define CL_HPP_TARGET_OPENCL_VERSION 200
+#include <chrono>
 #include <iostream>
-#include <fstream>
 #include <vector>
+#include <fstream>
+#include <string>
 #include <sstream>
 #include <CL/cl.h>
-#include <chrono>
-#include "mandelbrot.h"
+#include <libclew/ocl_init.h>
+const int SIZE_W_H = 1024;
+const int SIZE_WORK_GROUP = 256;
+
 
 std::string get_program_text() {
-    std::ifstream f("mandelbrot.cl"); // читаем код из файла
+    std::ifstream f("./mandelbrot.cl");
     std::stringstream ss;
     ss << f.rdbuf();
-    return ss.str(); // возвращаем
+    return ss.str();
 }
 
 
 cl_program build_program(cl_context ctx, cl_device_id device) {
-    std::string kernel_src = get_program_text(); // получить расположение файла cl
-    const char* src_text = kernel_src.data(); // получить путь файла
-    size_t src_size = kernel_src.size(); // получить размер строки
-    cl_program program = clCreateProgramWithSource(  // Создает программный объект для контекста
+    std::string kernel_src = get_program_text();
+    const char* src_text = kernel_src.data();
+    size_t src_size = kernel_src.size();
+    cl_program program = clCreateProgramWithSource(
         ctx,
         1,
         &src_text,
         &src_size,
         NULL
     );
-    clBuildProgram(program, 0, NULL, NULL, NULL, NULL); //Создать исполняемый файл программы
+    clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
     return program;
 }
 
-cl_device_id create_device() { // выбор устройство для использования
+cl_device_id create_device() {
     cl_platform_id platform;
     cl_device_id device;
     cl_uint platformsCount = 0;
-    clGetPlatformIDs(0, nullptr, &platformsCount); // получаем количество устройств
+    clGetPlatformIDs(0, nullptr, &platformsCount);
     std::vector<cl_platform_id> platforms(platformsCount);
-    clGetPlatformIDs(platformsCount, platforms.data(), nullptr); // получаем платформы
-    clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, 1, &device, NULL); // получаем устройство
+    clGetPlatformIDs(platformsCount, platforms.data(), nullptr);
+    clGetDeviceIDs(platforms[1], CL_DEVICE_TYPE_GPU, 1, &device, NULL);
     return device;
 }
 
-int align(int x, int y) { // сделать кратным число
+int align(int x, int y) {
     return (x + y - 1) / y * y;
 }
 
 
-void invoke_kernel(  // создаём kernel
+void invoke_kernel(
     cl_kernel kernel,
     cl_command_queue queue,
     cl_mem buffer,
     cl_uint* result,
     int w,
     int h,
-    float iters
+    float iters 
 ) {
-    clSetKernelArg(kernel, 0, sizeof(float), &iters); // инициализируем аргументы
+    clSetKernelArg(kernel, 0, sizeof(float), &iters);
     clSetKernelArg(kernel, 1, sizeof(cl_int), &w);
     clSetKernelArg(kernel, 2, sizeof(cl_int), &h);
     clSetKernelArg(kernel, 3, sizeof(cl_mem), &buffer);
-    size_t local_size[2] = { 256, 1 }; // задаём размер work group
-    size_t global_size[2] = {  // размер изображения, кратный размеру work group
-        align(w, local_size[0]), 
+    size_t local_size[2] = {SIZE_WORK_GROUP, 1};
+    size_t global_size[2] = {
+        align(w, local_size[0]),
         align(h, local_size[1])
     };
-    clEnqueueNDRangeKernel( // отдать команду на запуск kernel
+    clEnqueueNDRangeKernel(
         queue,
         kernel,
         2,
@@ -75,7 +78,7 @@ void invoke_kernel(  // создаём kernel
         NULL,
         NULL
     );
-    clEnqueueReadBuffer( // отдать команду на чтение результата
+    clEnqueueReadBuffer(
         queue,
         buffer,
         CL_TRUE,
@@ -86,10 +89,12 @@ void invoke_kernel(  // создаём kernel
         NULL,
         NULL
     );
-    clFinish(queue); // подождать завершение всех операций
+    clFinish(queue);
 }
-void save_result(const cl_uint* pixels, int w, int h, const char* fileName) {
-    std::ofstream file(fileName, std::ios::binary);
+
+
+void save_result(const cl_uint* pixels, int w, int h) {
+    std::ofstream file("result.ppm", std::ios::binary);
     file << "P6\n" << w << " " << h << "\n255\n";
     for (int y = 0; y < h; y++) {
         const cl_uint* line = pixels + w * y;
@@ -99,51 +104,49 @@ void save_result(const cl_uint* pixels, int w, int h, const char* fileName) {
     }
 }
 
-int main() {
-    static const int res_w = 4096, res_h = 4096; // определяем размер изображения
-    int max_iters = 50;
-    cl_device_id device = create_device(); // создаём устройство
-    cl_context ctx = clCreateContext(NULL, 1, &device, NULL, NULL, NULL); // создаём контекст
-    cl_program program = build_program(ctx, device); // вызываем функцию сборки программы
-    cl_kernel kernel = clCreateKernel(program, "mandelbrot", NULL); // получаем kernel из программы
-    cl_command_queue queue = clCreateCommandQueue(ctx, device, 0, NULL); // создаём очередь
 
+
+
+int main() {
+    if (!ocl_init()) throw;
+    static const int res_w = SIZE_W_H, res_h = SIZE_W_H;
+    cl_device_id device = create_device();
+    cl_context ctx = clCreateContext(NULL, 1, &device, NULL, NULL, NULL);
+    cl_program program = build_program(ctx, device);
+    cl_kernel kernel = clCreateKernel(program, "mandelbrot", NULL);
+    cl_command_queue queue = clCreateCommandQueue(ctx, device, 0, NULL);
+    
     cl_mem buffer = clCreateBuffer(
         ctx,
         CL_MEM_WRITE_ONLY,
         sizeof(cl_uint) * res_w * res_h,
         NULL,
         NULL
-    ); // создаём буффер для сохранения результата
+    );
 
-    std::vector<cl_uint> pixels(res_w * res_h); // выделить буффер 
-    auto startTime = std::chrono::steady_clock::now(); // засечь время начала
-    invoke_kernel( // запускаем отрисовку мандельброта
+    std::vector<cl_uint> pixels(res_w * res_h);
+    auto start_programm = std::chrono::high_resolution_clock::now();
+
+    invoke_kernel(
         kernel,
         queue,
         buffer,
         pixels.data(),
         res_w,
         res_h,
-        max_iters
-    ); 
+        50
+    );
 
-    std::chrono::duration<double> endTime = std::chrono::steady_clock::now() - startTime; // закончить время подсчёта
-    std::cout << "GPU Time: " << endTime.count() << " seconds" << std::endl;
+    auto end_programm = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_programm - start_programm);
+    std::cout << "GPU time: " << duration.count() << std::endl;
 
-    save_result(pixels.data(), res_w, res_h,"resultGPU.ppm"); // сохранить ppm файл
+    save_result(pixels.data(), res_w, res_h);
 
-    clReleaseKernel(kernel); // очистить данные
+    clReleaseKernel(kernel);
     clReleaseMemObject(buffer);
     clReleaseCommandQueue(queue);
     clReleaseProgram(program);
     clReleaseContext(ctx);
-    std::vector<cl_uint> pixelsCpu(res_w * res_h); // выделить буффер 
-    auto startTimeCpu = std::chrono::steady_clock::now(); // засечь время начала
-    drawMandelbrot(max_iters, res_w, res_h, pixelsCpu.data());
-    std::chrono::duration<double> endTimeCpu = std::chrono::steady_clock::now() - startTime; // закончить время подсчёта
-    std::cout << "CPU Time: " << endTimeCpu.count() << " seconds" << std::endl;
-    save_result(pixelsCpu.data(), res_w, res_h, "resultCPU.ppm");
     return 0;
 }
-
