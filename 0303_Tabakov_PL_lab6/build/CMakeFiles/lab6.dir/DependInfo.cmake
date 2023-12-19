1879e37fc07128bb09b34cdbe702e0cc806233ab
p.tabakov <p.tabakov@geoscan.aero>
2023-12-19 15:43:36 +0300

    done

---
+++ 0303_Tabakov_PL_lab6/build/CMakeFiles/lab6.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/liza/PA_lab6/main.cpp" "CMakeFiles/lab6.dir/main.cpp.o" "gcc" "CMakeFiles/lab6.dir/main.cpp.o.d"
+  "/home/liza/PA_lab6/matrix.cpp" "CMakeFiles/lab6.dir/matrix.cpp.o" "gcc" "CMakeFiles/lab6.dir/matrix.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/liza/PA_lab6/build/libs/clew/CMakeFiles/libclew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
